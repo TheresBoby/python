61a957889916cd7d3c385477f0a45b8d260acaa4
TheresBoby <95970791+TheresBoby@users.noreply.github.com>
2024-03-24 15:28:28 +0530

    Add files via upload

---
+++ sumof n.py
@@ -0,0 +1,7 @@
+x=int(input("enter no:"))
+i=1
+s=0
+while i<=x:
+    s=s+i
+    i+=1
+print("sum=",s)
---
+++ whileloop.py
@@ -0,0 +1,5 @@
+x=int(input("enter start:"))
+y=int(input("enter stop:"))
+while x<=y:
+    print(x)
+    x+=1
